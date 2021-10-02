32988c8b75d2e11050061a16522707966e3682eb
jsegobia0 <jsegobia0@yahoo.com>
2021-10-02 14:10:06 -0700

    updated file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,13 +8,13 @@ function Contact() {
       <p>
         If you would like to collaborate or have any questions, feel free to message me in the following links below.
       </p>
-      <Link to="https://github.com/jsegobia0" role="button" className="btn btn-link">
+      <Link to="https://github.com/jsegobia0" role="button" className="fab fa-github btn btn-link">
         Github
       </Link>
-      <Link to="https://www.linkedin.com/in/jonathan-segobia-544264213/" role="button" className="btn btn-link">
+      <Link to="https://www.linkedin.com/in/jonathan-segobia-544264213/" role="button" className="fab fa-linkedin btn btn-link">
         LinkedIn
       </Link>
-      <Link to="jsegobia0@yahoo.com" role="button" className="btn btn-link">
+      <Link to="jsegobia0@yahoo.com" role="button" className=" fab fa-yahoo btn btn-link">
         Yahoo
       </Link>
     </div>
